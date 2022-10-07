ebdbe375d1082008505583ce0d03e600ef55a761
Snoworange420 <110935731+Snoworange420@users.noreply.github.com>
2022-10-08 04:13:46 +0900

    Delete src/main directory

--- src/main/java/com/snoworange/mousse/event/listeners/EventRenderGUI.java
+++
@@ -1,7 +0,0 @@
-package com.snoworange.mousse.event.listeners;
-
-import com.snoworange.mousse.event.Event;
-
-public class EventRenderGUI extends Event<EventRenderGUI> {
-
-}
--- src/main/java/com/snoworange/mousse/module/setting/Setting.java
+++
@@ -1,8 +0,0 @@
-package com.snoworange.mousse.module.setting;
-
-import com.snoworange.mousse.module.Module;
-
-public abstract class Setting {
-    public String name;
-    public Module parent;
-}
